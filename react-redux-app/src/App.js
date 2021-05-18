e8b90abc68e96b74c313399443544977273f48bc
hws522 <sraphic@kakao.com>
2021-05-19 00:58:21 +0900

    create react component without redux

---
+++ react-redux-app/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'; // class 형식으로.
+import './App.css';
+import AddNumberRoot from './components/AddNumberRoot';
+import DisplayNumberRoot from './components/DisplayNumberRoot';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Root</h1>
+      <AddNumberRoot></AddNumberRoot>
+      <DisplayNumberRoot></DisplayNumberRoot>
+    </div>
+  );
+}
+
+export default App;
